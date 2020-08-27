deef1170b230155c2f9c8b047acecaca382da973
mimamulhassankhan <pappuhassan440@gmail.com>
2020-08-27 21:38:57 +0600

    Updates

---
+++ precache-manifest.3a1c8594025f4d8908d56d432fd547e6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8743fa93d74482daa0624492751a5c99",
+    "url": "/first-react-api-app/index.html"
+  },
+  {
+    "revision": "d7f45480f747a358e413",
+    "url": "/first-react-api-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5e97a74f9e38ab3bdf11",
+    "url": "/first-react-api-app/static/js/2.a297d3ed.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/first-react-api-app/static/js/2.a297d3ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7f45480f747a358e413",
+    "url": "/first-react-api-app/static/js/main.fcad1643.chunk.js"
+  },
+  {
+    "revision": "2d8fef165e5e617d64bb",
+    "url": "/first-react-api-app/static/js/runtime-main.1fa2469b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/first-react-api-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
